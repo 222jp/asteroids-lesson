892e214201cfd9e08afcb44b7e9dd6c592d6e22c
222jp <jordanrperry@zoho.com>
2025-09-22 22:50:54 -0700

    clock tick cleanup

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
         pygame.display.flip()
-        clock.tick(60)
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
